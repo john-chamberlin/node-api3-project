94b4c3eb593ddc409c126f82a6a0c8f0fe4152e5
John Chamberlin <johnchamberlin00@gmail.com>
2021-02-11 19:03:21 -0600

    trying to fix

--- api/server.js
+++ api/server.js
@@ -23,8 +23,5 @@ server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-server.use('/api/*', (_, res) => {
-  res.json({ data: 'api is working bro' })
-})
 
 module.exports = server;
